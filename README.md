8f3f5cd18c28e05e691cea545882e7f0249e43e7
senntyou <jiangjinbelief@163.com>
2019-03-29 08:51:14 +0800

    first version

--- README.md
+++ README.md
@@ -1,3 +1,129 @@
 # sdep
 
 See the dependencies tree of a module.
+
+## quick start
+
+Install sdep:
+
+```
+npm install sdep -g
+```
+
+Usage:
+
+```
+sdep [options] <file>
+```
+
+## options
+
+- `-q, --query <query>`: a query string to filter output
+- `-r, --regular`: regard query string as regular expression to filter output
+- `-i, --ignore`: ignore files under node_modules
+- `-d, --directory <directory>`: the directory containing all modules, default process.cwd()
+- `-b, --base <base>`: base path to shorten the output lines, default process.cwd()
+- `--rc <rc>`: RequireJs config for AMD modules
+- `--wc <wc>`: Webpack config for aliased modules
+- `--tc <tc>`: TypeScript config
+- `-f, --full`: show full dependencies chain when use a query string to filter output
+
+## used libraries
+
+- [commander.js](https://github.com/tj/commander.js)
+- [node-dependency-tree](https://github.com/dependents/node-dependency-tree)
+
+## examples
+
+#### see a file's dependencies hierarchy
+
+```
+sdep example/index.js
+```
+
+```
+example/index.js
+├ example/css/css.css
+├ example/css/scss.scss
+├ example/css/less.less
+├ example/jsx.jsx
+| ├ node_modules/react/index.js
+| | ├ node_modules/react/cjs/react.production.min.js
+| | | └ node_modules/object-assign/index.js
+| | └ node_modules/react/cjs/react.development.js
+| |   ├ node_modules/object-assign/index.js
+| |   └ node_modules/prop-types/checkPropTypes.js
+| |     └ node_modules/prop-types/lib/ReactPropTypesSecret.js
+| ├ node_modules/react-dom/index.js
+| | ├ node_modules/react-dom/cjs/react-dom.production.min.js
+| | | ├ node_modules/react/index.js
+| | | | ├ node_modules/react/cjs/react.production.min.js
+| | | | | └ node_modules/object-assign/index.js
+| | | | └ node_modules/react/cjs/react.development.js
+| | | |   ├ node_modules/object-assign/index.js
+| | | |   └ node_modules/prop-types/checkPropTypes.js
+| | | |     └ node_modules/prop-types/lib/ReactPropTypesSecret.js
+| | | ├ node_modules/object-assign/index.js
+| | | └ node_modules/scheduler/index.js
+| | |   ├ node_modules/scheduler/cjs/scheduler.production.min.js
+| | |   └ node_modules/scheduler/cjs/scheduler.development.js
+| | └ node_modules/react-dom/cjs/react-dom.development.js
+| |   ├ node_modules/react/index.js
+| |   | ├ node_modules/react/cjs/react.production.min.js
+| |   | | └ node_modules/object-assign/index.js
+| |   | └ node_modules/react/cjs/react.development.js
+| |   |   ├ node_modules/object-assign/index.js
+| |   |   └ node_modules/prop-types/checkPropTypes.js
+| |   |     └ node_modules/prop-types/lib/ReactPropTypesSecret.js
+| |   ├ node_modules/object-assign/index.js
+| |   ├ node_modules/prop-types/checkPropTypes.js
+| |   | └ node_modules/prop-types/lib/ReactPropTypesSecret.js
+| |   ├ node_modules/scheduler/index.js
+| |   | ├ node_modules/scheduler/cjs/scheduler.production.min.js
+| |   | └ node_modules/scheduler/cjs/scheduler.development.js
+| |   └ node_modules/scheduler/tracing.js
+| |     ├ node_modules/scheduler/cjs/scheduler-tracing.production.min.js
+| |     └ node_modules/scheduler/cjs/scheduler-tracing.development.js
+| └ example/wel.jsx
+|   └ node_modules/react/index.js
+|     ├ node_modules/react/cjs/react.production.min.js
+|     | └ node_modules/object-assign/index.js
+|     └ node_modules/react/cjs/react.development.js
+|       ├ node_modules/object-assign/index.js
+|       └ node_modules/prop-types/checkPropTypes.js
+|         └ node_modules/prop-types/lib/ReactPropTypesSecret.js
+└ example/async/index.js
+  └ example/async/index.css
+```
+
+#### see a file's dependencies chain which contains react
+
+```
+sdep example/index.js -q react
+```
+
+```
+example/index.js
+└ example/jsx.jsx
+  └ node_modules/react/index.js
+example/index.js
+└ example/jsx.jsx
+  └ node_modules/react-dom/index.js
+example/index.js
+└ example/jsx.jsx
+  └ example/wel.jsx
+    └ node_modules/react/index.js
+```
+
+#### see a file's dependencies chain which contains less or scss
+
+```
+sdep example/index.js -q 'less|scss' -r
+```
+
+```
+example/index.js
+└ example/css/scss.scss
+example/index.js
+└ example/css/less.less
+```
--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ const cwd = process.cwd();
 
 let basePath = cwd;
 
+/**
+ * Make `dependency-tree` config.
+ *
+ * @param file
+ * @param ignore
+ * @param directory
+ * @param rc
+ * @param wc
+ * @param tc
+ * @returns {{filename: *, nodeModulesConfig: {entry: string}, nonExistent: Array, directory: *}}
+ */
 function makeConfig(file, { ignore, directory, rc, wc, tc }) {
   const config = {
     filename: file,
@@ -26,16 +37,59 @@ function makeConfig(file, { ignore, directory, rc, wc, tc }) {
   return config;
 }
 
-function printTree(tree, level = 0) {
-  Object.keys(tree).forEach(key => {
-    console.log(' '.repeat(level * 2) + relative(basePath, key));
-    printTree(tree[key], level + 1);
+/**
+ * Prefix of one line.
+ *
+ * @param lastItems
+ * @returns {string}
+ */
+function printTreePrefix(lastItems) {
+  return lastItems
+    .map((last, index) => {
+      // first is always true
+      if (index === 0) return '';
+
+      return last ? '  ' : '| ';
+    })
+    .join('');
+}
+
+/**
+ * Print a tree.
+ *
+ * @param tree
+ * @param level
+ * @param lastItems
+ */
+function printTree(tree, level = 0, lastItems = []) {
+  const treeKeys = Object.keys(tree);
+  treeKeys.forEach((key, index) => {
+    const displayPath = relative(basePath, key);
+    const isLastItem = index === treeKeys.length - 1;
+
+    if (!level) console.log(displayPath);
+    else if (level === 1)
+      console.log(`${!isLastItem ? '├' : '└'} ${displayPath}`);
+    else {
+      console.log(
+        `${printTreePrefix(lastItems)}${
+          !isLastItem ? '├' : '└'
+        } ${displayPath}`,
+      );
+    }
+
+    printTree(tree[key], level + 1, [...lastItems, isLastItem]);
   });
 }
 
+/**
+ * Run command
+ *
+ * @param file
+ * @param options
+ */
 function run(file, options) {
-  // const {query, regular, base} = options;
-  const { query, base } = options;
+  const { query, regular, base, full } = options;
 
   if (base) basePath = base;
 
@@ -43,29 +97,77 @@ function run(file, options) {
 
   if (!query) {
     printTree(tree);
+    return;
   }
+
+  const treeList = [];
+  const queryRegExp = new RegExp(query);
+
+  function makeTreeList(t, chain = []) {
+    const treeKeys = Object.keys(t);
+
+    if (full && !treeKeys.length) {
+      treeList.push(chain);
+      return;
+    }
+
+    treeKeys.forEach(key => {
+      const displayKey = relative(basePath, key);
+      if (
+        !full &&
+        (regular ? queryRegExp.test(key) : key.indexOf(query) > -1)
+      ) {
+        treeList.push([...chain, displayKey]);
+        return;
+      }
+      makeTreeList(t[key], [...chain, displayKey]);
+    });
+  }
+
+  makeTreeList(tree);
+
+  let realTreeList = [];
+  if (full) {
+    realTreeList = treeList.filter(
+      item =>
+        !!item.find(i =>
+          regular ? queryRegExp.test(i) : i.indexOf(query) > -1,
+        ),
+    );
+  } else realTreeList = treeList;
+
+  realTreeList.forEach(item => {
+    item.forEach((i, index) => {
+      if (!index) console.log(i);
+      else console.log(`${'  '.repeat(index - 1)}└ ${i}`);
+    });
+  });
 }
 
 commander
   .version(pkg.version)
   .description('See the dependencies tree of a module')
   .arguments('<file>')
-  .option('-q, --query <query>', 'A query string to filter output')
+  .option('-q, --query <query>', 'a query string to filter output')
   .option(
     '-r, --regular',
-    'Regard query string as regular expression to filter output',
+    'regard query string as regular expression to filter output',
   )
-  .option('-i, --ignore', 'Ignore files under node_modules')
+  .option('-i, --ignore', 'ignore files under node_modules')
   .option(
     '-d, --directory <directory>',
-    'The directory containing all modules, default process.cwd()',
+    'the directory containing all modules, default process.cwd()',
   )
   .option(
     '-b, --base <base>',
-    'Base path to shorten the output lines, default process.cwd()',
+    'base path to shorten the output lines, default process.cwd()',
   )
   .option('--rc <rc>', 'RequireJs config for AMD modules')
   .option('--wc <wc>', 'Webpack config for aliased modules')
   .option('--tc <tc>', 'TypeScript config')
+  .option(
+    '-f, --full',
+    'show full dependencies chain when use a query string to filter output',
+  )
   .action(run)
   .parse(process.argv);
